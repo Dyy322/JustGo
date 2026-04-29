c384dac65d92a79be0c66ba160f554f20a33a552
北辰 <duyuyu@soulapp.cn>
2026-04-29 11:33:38 +0800

    Initial commit

---
+++ src/main/java/com/dyu/justgobackend/JustGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dyu.justgobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustGoBackendApplication.class, args);
+    }
+
+}
